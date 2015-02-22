c756f76e82d1bb1fea4246af63bef453913d156e
Евгений Шилин <euqen.kingston@yandex.ru>
2015-02-22 17:04:59 +0200

    Upload source and executive files of server and client applications.

---
+++ client-source.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace front_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver();
+        }
+
+        public static void Receiver() {
+
+            byte[] data;
+            Console.WriteLine("Starting server...");
+            const int SERVER_PORT = 8085;
+            const int CLIENT_PORT = 8086;
+
+            Socket Receive = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint Node = new IPEndPoint(IPAddress.Any, CLIENT_PORT);
+            Receive.Bind(Node);
+
+            IPEndPoint ServerNode = new IPEndPoint(IPAddress.Any, SERVER_PORT);
+            EndPoint RemoteNode = (EndPoint)ServerNode;
+            Console.WriteLine("Server started successfully.");
+            Console.WriteLine("Begin receiving current time....");
+            Thread.Sleep(3000);
+
+            while (true)
+            {
+                data = new byte[1024];
+                int count;
+                Console.WriteLine("Server is unavalible");
+                count =  Receive.ReceiveFrom(data, ref RemoteNode);
+                
+                Console.WriteLine(data.ToString());
+                string message = Encoding.ASCII.GetString(data);
+                Console.Clear();
+                Console.WriteLine(message);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ server-source.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        const int SERVER_PORT = 8085;
+        const int CLIENT_PORT = 8086;
+        static void Main(string[] args)
+        {
+            Broadcaster();
+
+        }
+
+        public static void Broadcaster()
+        {
+            byte[] data;
+            Console.WriteLine("Starting server...");
+            Socket Broadcast = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Broadcast.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            IPEndPoint Node = new IPEndPoint(IPAddress.Any, SERVER_PORT);
+            Broadcast.Bind(Node);
+
+            IPEndPoint ClientNode = new IPEndPoint(IPAddress.Broadcast, CLIENT_PORT);
+            EndPoint RemoteNode = (EndPoint)ClientNode;
+            Console.WriteLine("Begin sending current date...");
+             while (true) {
+
+                DateTime date = DateTime.Now;
+                data = Encoding.ASCII.GetBytes(date.ToString());
+             
+                Broadcast.SendTo(data, data.Length, SocketFlags.None, RemoteNode);
+                Thread.Sleep(1000);
+            }
+            
+        }
+        
+    }
+}
