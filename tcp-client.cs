5d6239e5ce22e731183aff784e3ce7f655e51efd
Евгений Шилин <euqen.kingston@yandex.ru>
2015-03-19 09:32:38 +0200

    New appl. TCP

---
+++ tcp-client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace front_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver();
+        }
+
+        public static void Receiver() {
+
+            Console.WriteLine("=======WELCOME TO CLIENT APPLICATION=======");
+            const int SERVER_PORT = 8085;
+            string IP;
+
+            Console.WriteLine("Server IP:");
+            IP = Console.ReadLine();
+
+            Socket TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint Node = new IPEndPoint(IPAddress.Parse(IP), SERVER_PORT);
+
+            try
+            {
+                byte[] bytes = new byte[1024];
+
+                while (true)
+                {
+                    TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    TCPSocket.Connect(Node);
+
+                    TCPSocket.Receive(bytes);
+                    string message = Encoding.ASCII.GetString(bytes);
+
+                    Console.WriteLine(message);
+                    Thread.Sleep(1000);
+                    Console.Clear();
+
+                    TCPSocket.Disconnect(false);
+                    Console.WriteLine(message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ooops! Something wrong! \n"  + e.ToString());
+                Console.Read();
+            }
+            finally 
+            {
+                TCPSocket.Shutdown(SocketShutdown.Both);
+                TCPSocket.Close();
+            }
+        }
+    }
+}
---
+++ tcp-server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        const int SERVER_PORT = 8085;
+        const int CLIENT_PORT = 8086;
+        static void Main(string[] args)
+        {
+            Broadcaster();
+
+        }
+
+        public static void Broadcaster()
+        {
+            byte[] data;
+            Console.WriteLine("Starting server...");
+            Socket TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint Node = new IPEndPoint(IPAddress.Any, SERVER_PORT);
+            TCPSocket.Bind(Node);
+
+            try
+            {
+                TCPSocket.Listen(10);
+                while (true)
+                {
+                   Socket handler = TCPSocket.Accept();
+                   Console.WriteLine(handler.LocalEndPoint.ToString() + " =>. Send date...");
+                   
+                   DateTime date = DateTime.Now;
+                   data = Encoding.ASCII.GetBytes(date.ToString());
+
+                   handler.Send(data);
+                   Thread.Sleep(1000);           
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ooops! Something wrong! " + e.ToString());
+                Console.ReadLine();
+            }
+            finally
+            {
+                TCPSocket.Shutdown(SocketShutdown.Both);
+                TCPSocket.Close();
+            }
+        }
+        
+    }
+}
